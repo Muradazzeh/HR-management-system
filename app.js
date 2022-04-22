03d8d592427eb2de634cf99d48de511fa43a439f
Muradazzeh <morad.azza@yahoo.com>
2022-04-21 21:18:52 -0700

    updatedsol

--- app.js
+++ app.js
@@ -1,59 +1,79 @@
 `use strict`
 let allEmp =[]
-function Employee(id,name,department,level,image,salary){
-    this.Employee_ID =id;
-    this.full_Name = name;
-    this.Department = department;
-   this.salary=salary
-    this.Level = level;
-    this.Image = image; 
-    allEmp.push(this.Level);
+let empsal=[]
+function Employee(Id,Name,Department,Level,image){
+    this.employee_ID =Id;
+    this.full_Name = Name;
+    this.department = Department;
+    this.level = Level;
+    this.image = image;
+    this.salary = 5;
+    empsal.push(this.salary)
+   allEmp.push(this)
     
-}console.log(allEmp)
-     Employee.prototype.rendar=function(){
-       document.write(`<h1>This is employee ${this.full_Name} and it is salary ${this.salary}: </h1> `);
-     }
+}
 
-    
+
+console.log(allEmp)
+
+function getRndInteger(min, max) {
+  return Math.round(Math.random() *  (max - min) ) + min ;
+} 
       
-      Employee.prototype.salary = function(){
-          for(let i=0;i<allEmp.length;i++){
-            if(allEmp[i]=="Senior"){
-               let  salary= 1500 + Math.random() * (2000 - 1500);
-                
-                }
-                else if( allEmp[i]=="Mid-Senior"){
-                   let salary= 1000 + Math.random() * (1500 - 1000);} 
-                else if( allEmp[i]=="Junior"){
-                   let salary= 500 + Math.random() * (1000 - 500);}
-      }console.log(this.salary)
-    }  
-            
-        
+      Employee.prototype.netsalary = function(){
+        if( this.level === "Senior"){
+          
+          this.salary=getRndInteger(1500,2000);
+          this.salary=this.salary * (0.925);
+         
+        }
+        if( this.level === "Mid-Senior"){
+          this.salary=getRndInteger(1000,1500);
+          this.salary=this.salary* (0.925);
+        }
+        if( this.level === "Junior"){
+          this.salary=getRndInteger(500,1000);
+         this.salary = this.salary * (0.925);
+          } 
+      }
+     
+    
+      console.log(empsal)
+ 
+      
 
+     
+    
 
+let employee1=new Employee(1000,'Ghazi Samer','Administration',"Senior");
+let employee2=new Employee(1001,'Lana Ali','Finance',"Senior");
+let employee3=new Employee(1002,'Tamara Ayoub','Marketing',"Senior");
+let employee4=new Employee(1003,'Safi Walid','Administration',"Mid-Senior");
+let employee5=new Employee(1004,'Omar Zaid','Development',"Senior");
+let employee6=new Employee(1005,'Rana Saleh','Development',"Junior");
+let employee7=new Employee(1006,'Hadi Ahmad','Finance',"Mid-Senior");
 
+Employee.prototype.rendar=function(){
+  document.write (`<h1>This is employee ${this.employee_ID} ${this.full_Name} and it is salary  : ${this.salary} </h1> `)}
+for(i=0;i<allEmp.length;i++){
+  allEmp[i].netsalary();
+  allEmp[i].rendar();
+  
+  
+}
 
-let employee1=new Employee('1000','Ghazi Samer','Administration','Senior',"./image",)
-let employee2=new Employee('1001','Lana Ali','Finance','Senior',"./image",)
-let employee3=new Employee('1002','Tamara Ayoub','Marketing','Senior',"./image",)
-let employee4=new Employee('1003','Safi Walid','Administration','Mid-Senior',"./image",)
-let employee5=new Employee('1004','Omar Zaid','Development','Senior',"./image",)
-let employee6=new Employee('1005','Rana Saleh','Development','Junior',"./image",)
-let employee7=new Employee('1006','Hadi Ahmad','Finance','Mid-Senior',"./image",)
-
-employee1.rendar();
-employee2.rendar();
-employee3.rendar();
-employee4.rendar();
-employee5.rendar();
-employee6.rendar();
-employee7.rendar();
-employee1.salary();
-employee2.salary();
-employee3.salary();
-employee4.salary();
-employee5.salary();
-employee6.salary();
-employee7.salary();
+// employee1.rendar();
+// employee2.rendar();
+// employee3.rendar();
+// employee4.rendar();
+// employee5.rendar();
+// employee6.rendar();
+// employee7.rendar();
+// employee1.netsalary() ;
+// employee2.netsalary();
+// employee3.netsalary();
+// employee4.netsalary();
+// employee5.netsalary();
+// employee6.netsalary();
+// employee7.netsalary();
 
