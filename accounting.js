5c7b61a1bd96e9044701c76e2431142a74d01935
Muradazzeh <morad.azza@yahoo.com>
2022-05-06 02:10:17 -0700

    finalsol

--- accounting.js
+++ accounting.js
@@ -1,16 +1,77 @@
 `use strict`
-let tableEl = document.getElementById("tab");
+let tableEl = document.getElementsByTagName("tbody")[0];
 
-
-Employee.prototype.renderTable = function () {
-    let tr = document.createElement("tr");
-    tableEl.appendChild(tr);
-
-    let depTd = document.createElement("td");
-    depTd.textContent = this.department;
-    tr.appendChild(depTd);
-
+depAllinfo = {
+    "Marketing": {
+        empCount: 0,
+        totSalary: 0,
+        avgSalary: 0,
+    },
+    "Finance": {
+        empCount: 0,
+        totSalary: 0,
+        avgSalary: 0,
+    },
+    "Development": {
+        empCount: 0,
+        totSalary: 0,
+        avgSalary: 0,
+    },
+    "Administration": {
+        empCount: 0,
+        totSalary: 0,
+        avgSalary: 0,
+    }
 }
-console.log(tableEl)
-    for (let i = 0; i < allEmp.length; i++) {
-          allEmp[i].renderTable();}
+
+
+for (let i = 0; i < allEmp.length; i++) {
+    let emp = allEmp[i];
+    emp.netsalary()
+    depAllinfo[emp.department].empCount += 1
+    depAllinfo[emp.department].totSalary += emp.salary
+    depAllinfo[emp.department].avgSalary = (depAllinfo[emp.department].totSalary / depAllinfo[emp.department].empCount)
+    console.table(emp)
+    console.table(depAllinfo[emp.department])
+}
+let totSum = 0;
+let avgSum = 0;
+for (key in depAllinfo) {
+    dept = depAllinfo[key];
+
+    avgSum += dept.avgSalary;
+    totSum += dept.totSalary;
+
+    addRow(key,  dept.empCount, dept.avgSalary, dept.avgSalary);
+}
+
+addRow("", "", avgSum, totSum, );
+
+
+
+function addRow(p1, p2, p3, p4,){
+    let tr = document.createElement("tr");
+    // if(cls)
+    //     tr.className=cls;
+
+    let depName = document.createElement("td");
+    depName.textContent = p1
+    tr.appendChild(depName);
+
+    let empCountEl = document.createElement("td");
+    empCountEl.textContent = p2;
+    tr.appendChild(empCountEl);
+
+
+
+    let avgSalaryEl = document.createElement("td");
+    avgSalaryEl.textContent = p3;
+    tr.appendChild(avgSalaryEl);
+
+
+    let totSalaryEl = document.createElement("td");
+    totSalaryEl.textContent = p4;
+    tr.appendChild(totSalaryEl);
+
+    tableEl.appendChild(tr);
+}
--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 
 `use strict`
 //// VARIABLES 
-let submtEL = document.getElementById("data")
 let allEmp = []
-function Employee(Id, Name, Department, Level, image,salr) {
+function Employee(Id, Name, Department, Level, image, salr) {
   this.employee_ID = Id;
   this.full_Name = Name;
   this.department = Department;
@@ -17,81 +16,80 @@ function Employee(Id, Name, Department, Level, image,salr) {
 /// CARD SECTION DISPLAY
 Employee.prototype.rendar = function () {
 
-    let gridItem = document.createElement("div");
-    gridItem.className = "grid-item";
-  
-    let imagEl = document.createElement("img")
-    imagEl.src = this.Image; imagEl.width = 250; imagEl.length = 250; imagEl.className += "ima"
-    console.log(gridItem)
-    gridItem.appendChild(imagEl)
-    let nameEL = document.createElement("p")
-    nameEL.textContent = `Name: ${this.full_Name} --  ID-# :  ${this.employee_ID} `
-    gridItem.appendChild(nameEL)
-    nameEL.style.color = "blue"
-    let idEl = document.createElement("p")
-    //idEl.textContent=`ID-# : ${this.employee_ID}`
-    gridItem.appendChild(idEl)
-  
-    let departmentEL = document.createElement("p")
-    departmentEL.textContent = `Department : ${this.department}  --  Level : ${this.level}`
-    gridItem.appendChild(departmentEL)
-    let levelEl = document.createElement("p")
-    //levelEl.textContent=`Level : ${this.level}`
-    gridItem.appendChild(levelEl)
-    let salaryEL = document.createElement("p")
-    salaryEL.textContent = `Salary : ${this.salary} $`
-    gridItem.appendChild(salaryEL)
-  
-  
-    document.getElementById("empList").appendChild(gridItem)
-  
-  }
-  //// SALARY CONDATION THREW THE CONSTRUCTOR
-  Employee.prototype.netsalary = function () {
-    if (this.level === "Senior") {
-  
-      this.salary = getRndInteger(1500, 2000);
-      // this.salary=this.salary * (0.925);
-  
-    }
-    if (this.level === "Mid-Senior") {
-      this.salary = getRndInteger(1000, 1500);
-      // this.salary=this.salary* (0.925);
-    }
-    if (this.level === "Junior") {
-      this.salary = getRndInteger(500, 1000);
-      //this.salary = this.salary * (0.925);
-    }
-  }
+  let gridItem = document.createElement("div");
+  gridItem.className = "grid-item";
+
+  let imagEl = document.createElement("img")
+  imagEl.src = this.Image; imagEl.width = 250; imagEl.length = 250; imagEl.className += "ima"
+  console.log(gridItem)
+  gridItem.appendChild(imagEl)
+  let nameEL = document.createElement("p")
+  nameEL.textContent = `Name: ${this.full_Name} --  ID-# :  ${this.employee_ID} `
+  gridItem.appendChild(nameEL)
+  nameEL.style.color = "blue"
+  let idEl = document.createElement("p")
+  //idEl.textContent=`ID-# : ${this.employee_ID}`
+  gridItem.appendChild(idEl)
+
+  let departmentEL = document.createElement("p")
+  departmentEL.textContent = `Department : ${this.department}  --  Level : ${this.level}`
+  gridItem.appendChild(departmentEL)
+  let levelEl = document.createElement("p")
+  //levelEl.textContent=`Level : ${this.level}`
+  gridItem.appendChild(levelEl)
+  let salaryEL = document.createElement("p")
+  salaryEL.textContent = `Salary : ${this.salary} $`
+  gridItem.appendChild(salaryEL)
 
 
-
-//// EMPLOYEE INSTANCE
-let employee1 = new Employee(1000, 'Ghazi Samer', 'Administration', "Senior", "./image/Ghazi.jpg");
-let employee2 = new Employee(1001, 'Lana Ali', 'Finance', "Senior", "./image/Lana.jpg");
-let employee3 = new Employee(1002, 'Tamara Ayoub', 'Marketing', "Senior", "./image/Tamara.jpg");
-let employee4 = new Employee(1003, 'Safi Walid', 'Administration', "Mid-Senior", "./image/Safi.jpg");
-let employee5 = new Employee(1004, 'Omar Zaid', 'Development', "Senior", "./image/Omar.jpg");
-let employee6 = new Employee(1005, 'Rana Saleh', 'Development', "Junior", "./image/Rana.jpg");
-let employee7 = new Employee(1006, 'Hadi Ahmad', 'Finance', "Mid-Senior", "./image/Hadi.jpg");
-
-
-
-
-
-//// CALLING FUNCTION THREW CONSTRUCTOR
-function rendarAll(){
-for (i = 0; i < allEmp.length; i++) {
-  allEmp[i].netsalary();
-  allEmp[i].rendar();
+  document.getElementById("empList").appendChild(gridItem)
 
 }
+//// SALARY CONDATION THREW THE CONSTRUCTOR
+Employee.prototype.netsalary = function () {
+  if (this.level === "Senior") {
+
+    this.salary = getRndInteger(1500, 2000);
+    // this.salary=this.salary * (0.925);
+
+  }
+  if (this.level === "Mid-Senior") {
+    this.salary = getRndInteger(1000, 1500);
+    // this.salary=this.salary* (0.925);
+  }
+  if (this.level === "Junior") {
+    this.salary = getRndInteger(500, 1000);
+    //this.salary = this.salary * (0.925);
+  }
 }
+
+
+if (getData() == null || getData().length == 0) {
+  //// EMPLOYEE INSTANCE
+  let employee1 = new Employee(1000, 'Ghazi Samer', 'Administration', "Senior", "./image/Ghazi.jpg");
+  let employee2 = new Employee(1001, 'Lana Ali', 'Finance', "Senior", "./image/Lana.jpg");
+  let employee3 = new Employee(1002, 'Tamara Ayoub', 'Marketing', "Senior", "./image/Tamara.jpg");
+  let employee4 = new Employee(1003, 'Safi Walid', 'Administration', "Mid-Senior", "./image/Safi.jpg");
+  let employee5 = new Employee(1004, 'Omar Zaid', 'Development', "Senior", "./image/Omar.jpg");
+  let employee6 = new Employee(1005, 'Rana Saleh', 'Development', "Junior", "./image/Rana.jpg");
+  let employee7 = new Employee(1006, 'Hadi Ahmad', 'Finance', "Mid-Senior", "./image/Hadi.jpg");
+}
+
 rendarAll()
+let submtEL = document.getElementById("data")
+submtEL.addEventListener("submit", handelSubmit)
+getData();
+
+
 
 //// Function AREA
+function rendarAll() {
+  for (i = 0; i < allEmp.length; i++) {
+    allEmp[i].netsalary();
+    allEmp[i].rendar();
 
-
+  }
+}
 function generate() {
   let id = () => {
     return Math.floor((1 + Math.random()) * 0x1000)
@@ -105,7 +103,6 @@ function generate() {
 function getRndInteger(min, max) {
   return Math.round(Math.random() * (max - min)) + min;
 }
-submtEL.addEventListener("submit", handelSubmit)
 function handelSubmit(event) {
   event.preventDefault();
 
@@ -115,13 +112,14 @@ function handelSubmit(event) {
   let empSal = document.getElementById(this.salary);
   let empName = event.target.fullName.value;
   let empImg = event.target.imag.value;
-  
+
   let newEmployee = new Employee(empId, empName, empDep, empLev, empImg, empSal);
   newEmployee.netsalary();
-  
+
   saveData(allEmp);
-  allEmp=[];
+
   newEmployee.rendar();
+  allEmp = [];
 }
 
 function saveData(data) {
@@ -135,24 +133,24 @@ function getData() {
   let arrayData = JSON.parse(retrievedData);
   // each object doesn't has access to render method
   if (arrayData != null) {
-    allEmp=[];
+    allEmp = [];
     for (let i = 0; i < arrayData.length; i++) {
-      
-      new Employee(arrayData[i].employee_ID, arrayData[i].full_Name, arrayData[i].department, arrayData[i].level , arrayData[i].Image, arrayData[i].salary);
-     
-    } 
-    
+
+      new Employee(arrayData[i].employee_ID, arrayData[i].full_Name, arrayData[i].department, arrayData[i].level, arrayData[i].Image, arrayData[i].salary);
+
+    }
+
     // for (let i = 0; i < allEmp.length; i++) {
     //   allEmp[i].rendar();
 
-      
-       
+
+
     // }
-   rendarAll();
+    // rendarAll();
     console.log(arrayData);
+    return allEmp;
   }
 
 }
 
 
-getData();
